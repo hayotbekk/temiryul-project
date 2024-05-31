2b52ed3f70eaf56af16ed03c7e5a2ff66f5f132a
amirbekk7 <normurodovamirbek730@gmail.com>
2024-05-31 12:35:15 +0300

    hayot-finish

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React from 'react'
 import Header from './components/Header/Header'
 import Hero from './components/Hero'
-<<<<<<< HEAD
 import Footer from './components/Footer'
 import Malumot from './components/malumot/malumot'
-=======
 import Footer from './components/Footer/Footer'
 import Xizmatlar from './components/Xizmatlar/Xizmatlar'
-
->>>>>>> 78b1420bdd72939c0da061c8c0ce0ee1357a9818
 
 const App = () => {
   return (
     <div>
-      <Header/>
-      <Hero/>
-<<<<<<< HEAD
+      <Header />
+      <Hero />
       <Malumot/>
-=======
       <Xizmatlar/>
->>>>>>> 78b1420bdd72939c0da061c8c0ce0ee1357a9818
       <Footer/>
-    </div>
+    </div >
   )
 }
 
