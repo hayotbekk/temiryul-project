53332c0e89383a75bfff7a702ae27d15c7ea3d75
Abdulvohis <abdulvohidcode@gmail.com>
2024-05-31 11:59:51 +0300

    bag--

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -4,16 +4,6 @@ import { Splide, SplideSlide } from '@splidejs/react-splide';
 const Hero = () => {
   return (
     <div className=''>
-      <main>
-        <section id="landing_page_hero_home">
-          <div className="relative w-full overflow-x-hidden flex h-[100vh]">
-            <div className="w-full h-full inline-block flex-none" style="left: 0%; right: 0%; transform: translateX(-2526px) translateZ(0px);">
-              <div className="relative h-full w-full">
-              </div>
-            </div>
-          </div>
-        </section>
-      </main>
       
     </div>
   )
