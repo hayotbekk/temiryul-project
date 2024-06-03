abdd5addf092e633b14cccad15d23b4d0c277369
Abdulvohis <abdulvohidcode@gmail.com>
2024-06-03 16:40:19 +0300

    log in deyarli bulli

--- src/Login/LoginPage/LoginPage.jsx
+++ src/Login/LoginPage/LoginPage.jsx
@@ -1,8 +1,32 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { apiUrl, token } from '../../api/api';
 
 const LoginPage = () => {
     const [phoneNumber, setPhoneNumber] = useState('');
     const [password, setPassword] = useState('');
+    const [role, setRole] = useState('/admin');
+    // const [tools, setTools] = useState(null)
+
+    // useEffect(() => {
+    //     getTools()
+    // }, [])
+    // const getTools = () => {
+    //     axios.get(`${apiUrl}auth-controller/login`, {
+    //         headers: {
+    //             'Authorization': `Bearer ${token}`
+    //         }
+    //     })
+    //         .then((res) => {
+    //             setTools(res)
+    //         })
+    //         .catch((err) => {
+    //             console.log(err);
+    //         })
+    // }
+
+    // console.log(tools)
 
     const user = {
         number: '9988890037',
@@ -11,18 +35,20 @@ const LoginPage = () => {
     const handleNumberChange = (e) => {
         setPhoneNumber(e.target.value);
     };
-
+    const navigate = useNavigate()
     const handlePasswordChange = (e) => {
         setPassword(e.target.value);
+
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (phoneNumber === user.number && password === user.password)  return(
-            alert('Login Successful')
-        )
+        if (phoneNumber === user.number && password === user.password) {
+            setRole(role);
+            navigate("/admin");
+        }
         else alert('Nmadur xato bor');
-        
+
     };
 
     return (
@@ -40,14 +66,14 @@ const LoginPage = () => {
                             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
                                 <div>
                                     <label htmlFor="numberInp" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Number</label>
-                                    <input 
+                                    <input
                                         type="text"
                                         id="numberInp"
                                         className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                                         placeholder="998"
                                         value={phoneNumber}
                                         onChange={handleNumberChange}
-                                    />  
+                                    />
                                     <span className='text-slate-500'>Iltimos raqam 998 bilan boshlansin</span>
                                 </div>
                                 <div className='py-4'>
